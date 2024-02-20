eb1810f59d9e44a552b2100d6659a3a4491a1919
h4-rahman <hamidur2.rahman@live.uwe.ac.uk>
2024-02-20 12:35:49 +0000

    Docker Environment consisting of Tailwind and MySQL

---
+++ myproject/entrypoint.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Apply database migrations
+echo "Making migrations"
+python manage.py makemigrations
+
+echo "Applying migrations"
+python manage.py migrate
+
+python manage.py runserver 0.0.0.0:8000
