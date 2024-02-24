8f598ff6c516be895500997428404f3db44e994d
Kacper Drupisz <kacper.drupisz@gmail.com>
2024-02-24 15:03:00 +0000

    pages

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,8 +1,15 @@
 from django.urls import path
 from .views import index
 from .views import users
+from .views import maintenance
+from .views import handler404
+from .views import handler500
 
 urlpatterns = [
     path('', index, name='index'),
-    path('user/',users)
+    path('user/',users),
+    path('404/', handler404),
+    path('500/', handler500),
+    path('maintenance/', maintenance)
 ]
+
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.template import RequestContext
 
 def index(request):
     return render(request, 'index.html')
 
 def users(request):
     return render(request, 'user_page.html')
+
+def handler404(request, *args, **kwargs):
+    response = render(request, '404.html', {})
+    response.status_code = 404
+    return response
+
+def handler500(request, *args, **kwargs):
+    response = render(request, '500.html', {})
+    response.status_code = 500
+    return response
+
+def maintenance(request):
+    return render(request, 'maintenance.html')
