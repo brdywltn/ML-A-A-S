33a9a8ea8a7ab9fea152744c7bd06c507d319018
Brody Wilton <brodywilton@Brodys-MacBook-Pro.local>
2024-02-26 15:56:37 +0000

    comment out all models because they're not ready yet

--- myproject/myapp/models.py
+++ myproject/myapp/models.py
@@ -8,42 +8,42 @@ from django.db import models
 #   3 - Accountant
 
 # Create your models here.
-class User(models.Model):
-    """
-    *   User model
-    """
-    username = models.CharField(max_length=150)
-    password = models.CharField(max_length=16)
-    email = models.EmailField(max_length=200)
-    usertype = models.ForeignKey("UserType")
+# class User(models.Model):
+#     """
+#     *   User model
+#     """
+#     username = models.CharField(max_length=150)
+#     password = models.CharField(max_length=16)
+#     email = models.EmailField(max_length=200)
+#     #usertype = models.ForeignKey("UserType")
 
-class UserType(models.Model):
-    """
-    *   Usertype model
-    """
-    usertype = models.CharField(max_length=15)
+# class UserType(models.Model):
+#     """
+#     *   Usertype model
+#     """
+#     usertype = models.CharField(max_length=15)
 
-class Logs(models.Model):
-    """
-    *   Logs model
-    """
-    user_id = models.ForeignKey("User")
-    content = models.CharField(max_length=2000)
-    date = models.DateTimeField()
+# class Logs(models.Model):
+#     """
+#     *   Logs model
+#     """
+#     content = models.CharField(max_length=2000)
+#     #user_id = models.ForeignKey("User")
+#     date = models.DateTimeField()
 
-class Feedback(models.Model):
-    """
-    *   Feedback Model
-    """
-    user_id = models.ForeignKey("User")
-    content = models.CharField(max_length=2000)
-    date = models.DateTimeField()
+# class Feedback(models.Model):
+#     """
+#     *   Feedback Model
+#     """
+#     #user_id = models.ForeignKey("User")
+#     content = models.CharField(max_length=2000)
+#     date = models.DateTimeField()
 
-class Bills(models.Model):
-    """
-    *   Bill/receipts Model
-    """
-    user_id = models.ForeignKey("User")
-    date = models.DateTimeField()
-    paid = models.BooleanField(default=False)
+# class Bills(models.Model):
+#     """
+#     *   Bill/receipts Model
+#     """
+#     #user_id = models.ForeignKey("User")
+#     date = models.DateTimeField()
+#     paid = models.BooleanField(default=False)
 
