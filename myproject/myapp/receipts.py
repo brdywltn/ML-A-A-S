91b49fe7d49c077e7fc0957e29968c73cb688297
Brody Wilton <brodywilton@192.168.1.227>
2024-03-01 23:28:39 +0000

    Add file for receipts generation

---
+++ myproject/myapp/receipts.py
@@ -0,0 +1,8 @@
+# We want to generate receipts for user monthly billing.
+# We want the function to accept a user
+# It should return the user's username, blanked out details and cost
+from fpdf import FPDF
+
+class PDF(FPDF):
+    pass # do nothing when it's executed
+
