a95c92f027dc1664fd36136eb09873e727db6c72
James <james39.smith@live.uwe.ac.uk>
2024-03-12 12:53:03 +0000

    fixing merge issues

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,12 +1,10 @@
 from django.urls import path
-<<<<<<< myproject/myapp/urls.py
 from .views import index, users, maintenance, handler404, handler500, register, user_login, terms_conditions, privacy_policy, handling_music_file, pricing, generate_pdf
 
 
 urlpatterns = [
     # path('', index, name='index'), <- uncomment when index/main page will be ready
     path('', index),
-
     path('user/', users, name='users'),
     path('404/', handler404),
     path('500/', handler500),
@@ -17,7 +15,7 @@ urlpatterns = [
     path('pricay_policy/', privacy_policy, name='privacy_policy'),
     path('pricing/', pricing, name='pricing'),
     path('uploading_file/', handling_music_file, name='uploading_file'),
-    path('generate_pdf/', generate_pdf, name='generate_pdf')
+    path('generate_pdf/', generate_pdf, name='generate_pdf'),
     path('pricing/', pricing, name='pricing'),
     path('generate_pdf/', generate_pdf, name='generate_pdf')
 ]
