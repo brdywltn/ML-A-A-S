4931b512e3980b0f3e71e74836adc7a21ef67b3c
Brody Wilton <brodywilton@hotmail.com>
2024-03-19 10:24:15 +0000

    Implement password reset on dashboard

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .views import index, users, maintenance, handler404, handler500, register, user_login, terms_conditions, privacy_policy, handling_music_file, pricing, generate_pdf, admin_table
-
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     # path('', index, name='index'), <- uncomment when index/main page will be ready
@@ -18,5 +18,7 @@ urlpatterns = [
     path('generate_pdf/', generate_pdf, name='generate_pdf'),
     path('pricing/', pricing, name='pricing'),
     path('generate_pdf/', generate_pdf, name='generate_pdf'),
-    path('admin_table/', admin_table, name='admin_table')
+    path('admin_table/', admin_table, name='admin_table'),
+    path('password_change/', auth_views.PasswordChangeView.as_view(template_name='password_change_form.html'), name='password_change'),
+    path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
 ]
